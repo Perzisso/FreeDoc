f2d6fe98b74808348f9fdb176ad42f523c7f02ed
Florian <florian.sasso@outlook.fr>
2023-02-09 17:37:35 +0100

    Seed ok / BDD full

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,30 +11,43 @@ require 'date'
 Patient.destroy_all
 Doctor.destroy_all
 Appointment.destroy_all
+City.destroy_all
+Specialty.destroy_all
 
+specialtys = ["Obstétrique", "Gatro", "Générale", "Chirurgie", "Pré-natale", "Radiologue", "Kiné", "Rénimation", "Oncologue", "Urologue"]
 
-specialty = ["Obstétrique", "Générale", "Chirurgie", "Gastro"]
+10.times do
+  city = City.create!(city_name: Faker::Nation.capital_city)
+end
 
-20.times do
+10.times do 
+  specialty = Specialty.create(specialty: specialtys[rand(0..9)])
+end
+
+10.times do |t|
   doctor = Doctor.create!(
                         firts_name: Faker::Name.first_name,
                         last_name: Faker::Name.last_name,
-                        specialty: specialty[rand(0..3)],
-                        zip_code: Faker::Number.between(from: 10000, to: 99999))
+                        zip_code: Faker::Number.between(from: 10000, to: 99999),
+                        city_id: t+1,
+                        specialty_id: t+1)
 end
 
-20.times do
+10.times do |t|
     patient = Patient.create!(
                         firts_name: Faker::Name.first_name,
-                        last_name: Faker::Name.last_name)
+                        last_name: Faker::Name.last_name,
+                        city_id: t+1)
 end
 
-20.times do |t|
+10.times do |t|
     appointment = Appointment.create(
                         date: DateTime.new(2023,2,18),
                         doctor_id: t+1,
                         patient_id: t+1)
 end
+
+
 
 
 
