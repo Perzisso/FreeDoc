dbf95051951039ae1cf2e1bc64162d51fada0a11
Florian <florian.sasso@outlook.fr>
2023-02-09 15:04:12 +0100

    Fonctionnal seed who add 20 Doctor/Patient/Appointment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+require 'date'
+
+Patient.destroy_all
+Doctor.destroy_all
+Appointment.destroy_all
+
+
+specialty = ["Obstétrique", "Générale", "Chirurgie", "Gastro"]
+
+20.times do
+  doctor = Doctor.create!(
+                        firts_name: Faker::Name.first_name,
+                        last_name: Faker::Name.last_name,
+                        specialty: specialty[rand(0..3)],
+                        zip_code: Faker::Number.between(from: 10000, to: 99999))
+end
+
+20.times do
+    patient = Patient.create!(
+                        firts_name: Faker::Name.first_name,
+                        last_name: Faker::Name.last_name)
+end
+
+20.times do |t|
+    appointment = Appointment.create(
+                        date: DateTime.new(2023,2,18),
+                        doctor_id: t+1,
+                        patient_id: t+1)
+end
+
+
+
+
